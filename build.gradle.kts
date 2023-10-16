ee4efd88e7ea476536f75d01395b020fff3d8e66
muhammadasrul <asrulajipangestu@gmail.com>
2023-10-16 11:59:05 +0700

    downgrade agp version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+buildscript {
+    val agp_version by extra("8.1.2")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0-beta06" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
